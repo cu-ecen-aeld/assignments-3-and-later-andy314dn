eac5f257e9d41ecc5286123050d6f9f99f15f740
andy314dn-carbon <andy.314.dn@gmail.com>
2024-10-11 22:32:20 +0700

    add code for solution 1

--- student-test/assignment1/Test_validate_username.c
+++ student-test/assignment1/Test_validate_username.c
@@ -18,5 +18,8 @@ void test_validate_my_username()
      * TODO: Replace the line below with your code here as described above to verify your /conf/username.txt 
      * config file and my_username() functions are setup properly
      */
-    TEST_ASSERT_TRUE_MESSAGE(false,"AESD students, please fix me!");
+    char *c1 = malloc_username_from_conf_file();
+    const char *c2 = my_username();
+    TEST_ASSERT_EQUAL_STRING_MESSAGE(c1, c2, "Names did not match!");
+    free(c1);
 }
