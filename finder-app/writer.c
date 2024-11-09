90020b3ec35114ac99cc48ff100386927d364ab2
Phuoc Nguyen gitbash <andy.314.dn@gmail.com>
2024-11-09 14:15:40 +0700

    add code

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -12,8 +12,45 @@
  * Use the syslog capability to log any unexpected errors with LOG_ERR level.
  */
 
-int main() {
-    openlog("AELD-Assignment1", 0, LOG_USER);
-    syslog(LOG_ERR, "For error: \n\r");
-    syslog(LOG_DEBUG, "Debug message\r\n");
+int main(int argc, char *argv[]) {
+    // open connection to syslog
+    openlog("AELD-Assignment1", LOG_PID | LOG_CONS, LOG_USER);
+
+    // check the number of arguments
+    if (argc != 3) {
+        syslog(LOG_ERR, "Invalid number of arguments. Usage: %s <writefle> <writestr>", argv[0]);
+        fprintf(stderr, "Error: Invalid number of arguments.\n");
+        closelog();
+        return 1;
+    }
+
+    char *writefile = argv[1];
+    char *writestr = argv[2];
+
+    // open file for writing
+    // (create if it does not exist, truncate if it does)
+    int fd = open(writefile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    if (-1 == fd) {
+        syslog(LOG_ERR, "Failed to open file %s: %s", writefile, strerror(errno));
+        fprintf(stderr, "Error: Could not open or create file!\n");
+        closelog();
+        return 1;
+    }
+
+    // write the string to file
+    ssize_t num_bytes_written = write(fd, writestr, strlen(writestr));
+    if (-1 == num_bytes_written) {
+        syslog(LOG_ERR, "Failed to write to file %s: %s", writefile, strerror(errno));
+        fprintf(stderr, "Error: Could not write to file!\n");
+        close(fd);
+        closelog();
+        return 1;
+    }
+
+    // log a debug message indicating what was written
+    syslog(LOG_DEBUG, "Writing %s to %s.", writestr, writefile);
+
+    // close opened files
+    close(fd);
+    closelog();
 }
